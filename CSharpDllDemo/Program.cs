be7fe2ee4af87a19a6e2cf194b2bd6143f3c85fb
Elase Huang <elase.huang@outlook.com>
2024-01-25 21:02:07 +0800

    init

---
+++ CSharpDllDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CSharpDllDemo;
+
+internal class Program
+{
+    [DllImport("Library/DllDemo.dll")]
+    private static extern int fireman_add(int a, int b);
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+
+        var result = fireman_add(10, 15);
+        Console.WriteLine(result);
+    }
+}
