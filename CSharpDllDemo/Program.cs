6e15f9078f3df8b147f18a6735cfbd7a0ea9deb6
elase huang <elase.huang@outlook.com>
2024-01-26 11:56:37 +0800

    build: simplify different platform build

--- CSharpDllDemo/Program.cs
+++ CSharpDllDemo/Program.cs
@@ -7,15 +7,7 @@ internal class Program
     private const string NativeLibraryWindows = "Library/DllDemo.dll";
     private const string NativeLibraryMacos = "Library/libDllDemo.dylib";
 
-#if WINDOWS
-            const string NativeLibrary = NativeLibraryWindows;
-#elif MACOS
-    private const string NativeLibrary = NativeLibraryMacos;
-#else
-            const string NativeLibrary = NativeLibraryWindows;
-#endif
-
-    [DllImport(NativeLibrary)]
+    [DllImport(NativeLibraryMacos)]
     private static extern int fireman_add(int a, int b);
 
     private static void Main(string[] args)
