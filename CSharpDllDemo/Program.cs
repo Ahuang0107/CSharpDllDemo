95a5d116d6fef1c377e70b8a1ba07b186086ae71
elase huang <elase.huang@outlook.com>
2024-01-26 13:02:53 +0800

    feat: create object through dll

--- CSharpDllDemo/Program.cs
+++ CSharpDllDemo/Program.cs
@@ -4,17 +4,30 @@ namespace CSharpDllDemo;
 
 internal class Program
 {
-    private const string NativeLibraryWindows = "Library/DllDemo.dll";
-    private const string NativeLibraryMacos = "Library/libDllDemo.dylib";
+    private const string NativeLibraryWindows = "../rust_dll_demo/target/release/DllDemo.dll";
+    private const string NativeLibraryMacos = "../rust_dll_demo/target/release/libDllDemo.dylib";
 
-    [DllImport(NativeLibraryMacos)]
+    private const string NativeLibrary = NativeLibraryMacos;
+
+    [DllImport(NativeLibrary)]
     private static extern int fireman_add(int a, int b);
+
+    [DllImport(NativeLibrary)]
+    private static extern IntPtr new_scene(ulong id);
+
+    [DllImport(NativeLibrary)]
+    private static extern ulong get_id(IntPtr scene);
 
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello World!");
 
-        var result = fireman_add(10, 15);
-        Console.WriteLine(result);
+        var addResult = fireman_add(10, 15);
+        Console.WriteLine($"Add Result: {addResult}");
+
+        var scene = new_scene(42);
+
+        var id = get_id(scene);
+        Console.WriteLine($"Scene ID: {id}");
     }
 }
--- rust_dll_demo/src/lib.rs
+++ rust_dll_demo/src/lib.rs
@@ -2,3 +2,28 @@
 pub extern "C" fn fireman_add(a: i32, b: i32) -> i32 {
     a + b
 }
+
+#[repr(C)]
+pub struct Scene {
+    pub id: u64,
+}
+
+impl Scene {
+    fn new(id: u64) -> Self {
+        Self { id }
+    }
+    fn get_id(&self) -> u64 {
+        self.id
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn new_scene(id: u64) -> *mut Scene {
+    Box::into_raw(Box::new(Scene::new(id)))
+}
+
+#[no_mangle]
+pub extern "C" fn get_id(my_struct: *const Scene) -> u64 {
+    let my_struct = unsafe { &*my_struct };
+    my_struct.get_id()
+}
