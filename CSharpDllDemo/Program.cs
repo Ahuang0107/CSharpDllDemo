9e422da21c3d94a46127bcab067be042c53a85f5
elase huang <elase.huang@outlook.com>
2024-01-26 11:50:26 +0800

    build: windows and macos platform use different dll file

--- CSharpDllDemo/Program.cs
+++ CSharpDllDemo/Program.cs
@@ -4,7 +4,18 @@ namespace CSharpDllDemo;
 
 internal class Program
 {
-    [DllImport("Library/DllDemo.dll")]
+    private const string NativeLibraryWindows = "Library/DllDemo.dll";
+    private const string NativeLibraryMacos = "Library/libDllDemo.dylib";
+
+#if WINDOWS
+            const string NativeLibrary = NativeLibraryWindows;
+#elif MACOS
+    private const string NativeLibrary = NativeLibraryMacos;
+#else
+            const string NativeLibrary = NativeLibraryWindows;
+#endif
+
+    [DllImport(NativeLibrary)]
     private static extern int fireman_add(int a, int b);
 
     private static void Main(string[] args)
